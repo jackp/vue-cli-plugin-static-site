401638e0d8edc49acb77a011753af187fde20dae
Jackson Parker <jack@rethinq.io>
2018-09-17 19:25:43 -0400

    [cleanup] Delete boilerplate

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,5 @@
 @import "./styles/global.scss";
 </style>
 
-
 <style lang="scss" scoped>
-#app {
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -2,20 +2,17 @@
   <view-template>
     <div class="home">
       <img alt="Vue logo" src="@/assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
   </view-template>
 </template>
 
 <script>
 import ViewTemplate from "@/components/templates/ViewTemplate.vue";
-import HelloWorld from "@/components/organisms/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    ViewTemplate,
-    HelloWorld
+    ViewTemplate
   }
 };
 </script>
