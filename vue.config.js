1d21b221ac5204e93182ae1a5a05823074513f4a
Jackson Parker <jack@rethinq.io>
2018-09-17 17:25:56 -0400

    [organization] Move posts config to vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+/**
+ * @vue/cli Configuration
+ * - https: //cli.vuejs.org/config/#vue-config-js
+ */
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: {
+          autoprefixer: {}
+        }
+      }
+    }
+  }
+};
